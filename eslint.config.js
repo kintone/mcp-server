6ff89bffe8183b15cb3192858dc89332ff16e87c
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-07-17 16:59:09 +0900

    chore: configure npm project

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import presetsNodeTypescriptPrettier from "@cybozu/eslint-config/flat/presets/node-typescript-prettier.js";
+import eslintPluginPackageJson from "eslint-plugin-package-json";
+import globals from "globals";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  ...presetsNodeTypescriptPrettier,
+  {
+    ignores: ["lib", "esm", "umd", "dist"],
+  },
+  {
+    files: ["*.cjs", "*.cts"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    files: ["*.js", "*.ts", "*.mjs", "*.mts"],
+    languageOptions: {
+      globals: globals.nodeBuiltin,
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
+  {
+    ...eslintPluginPackageJson.configs.recommended,
+    rules: {
+      ...eslintPluginPackageJson.configs.recommended.rules,
+      "@typescript-eslint/consistent-type-imports": "off",
+    },
+  },
+];
