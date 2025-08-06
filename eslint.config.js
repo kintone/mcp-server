40d98c1609a9e39536b0460e73271bc069575665
Masaharu Tashiro <tasshi.me@gmail.com>
2025-08-06 18:57:32 +0900

    fix `n/hashbang` rule setting

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,11 @@ export default [
   {
     rules: {
       "@typescript-eslint/consistent-type-imports": "error",
+      "n/hashbang": [
+        "error",
+        // https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/hashbang.md#convertpath
+        { convertPath: { "src/**/*.ts": ["^src/(.+?)\\.ts$", "dist/$1.js"] } },
+      ],
     },
   },
   {
