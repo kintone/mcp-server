bcbb4c8f4d46cbb8068aaf89813bfd5867bf965d
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2025-08-07 11:24:51 +0900

    chore: remove example add tool (#19)

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,4 +1,3 @@
-import { add } from "./examples/add.js";
 import { addRecords } from "./kintone/add-records.js";
 import { getApp } from "./kintone/get-app.js";
 import { getApps } from "./kintone/get-apps.js";
@@ -10,7 +9,6 @@ import type { Tool } from "./types.js";
 import { updateRecords } from "./kintone/update-records.js";
 
 export const tools: Array<Tool<any, any>> = [
-  add,
   addRecords,
   getApp,
   getApps,
