3bbcb0ee226508f461bb35b826fc04e455b9b82c
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2025-08-18 16:11:39 +0900

    feat: change server life cycle

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { server } from "./server.js";
+import { createServer } from "./server.js";
 import { parseKintoneClientConfig } from "./config.js";
+import { getKintoneClient } from "./client.js";
 
 const main = async () => {
   const transport = new StdioServerTransport();
   console.error("Starting server...");
   parseKintoneClientConfig();
+  const server = createServer();
   await server.connect(transport);
 };
 
--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,22 @@ import { tools } from "./tools/index.js";
 import { version } from "./version.js";
 import { PACKAGE_NAME } from "./config.js";
 
-const server = new McpServer(
-  {
-    name: PACKAGE_NAME,
-    version,
-  },
-  {
-    capabilities: {
-      tools: {},
+export const createServer = (): McpServer => {
+  const server = new McpServer(
+    {
+      name: PACKAGE_NAME,
+      version,
     },
-  },
-);
+    {
+      capabilities: {
+        tools: {},
+      },
+    },
+  );
 
-tools.forEach((tool) => {
-  server.registerTool(tool.name, tool.config, tool.callback);
-});
+  tools.forEach((tool) => {
+    server.registerTool(tool.name, tool.config, tool.callback);
+  });
 
-export { server };
+  return server;
+};
