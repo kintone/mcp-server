a77e08acdfe16dafc55d417756ea12af28034487
Takahiko Kawakami <53943902+shabaraba@users.noreply.github.com>
2026-07-10 09:20:29 +0900

    chore: remove unused baseFieldProperties and field-properties.ts (#497)

--- src/schema/app/index.ts
+++ src/schema/app/index.ts
@@ -1,3 +1,2 @@
-export { baseFieldProperties } from "./field-properties.js";
 export { layoutForParameterSchema } from "./form-layout.js";
 export { propertiesForParameterSchema } from "./properties-parameter.js";
